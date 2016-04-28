69d85c66fdbc21d2c0f1edba7868200a4cb9e8b6
voshawn <svo@inbox.com>
2016-04-28 11:20:47 -0700

    edit shell

--- my_scraper/scraper_app/wanderu/busscraper.sh
+++ my_scraper/scraper_app/wanderu/busscraper.sh
@@ -2,6 +2,6 @@
 
 # be sure to change both virtualenv directory and scrape/living_social
 # directory to where your venv and code is.
-source $WORKON_HOME/busscraper/bin/activate
+source ~/.virtualenvs/busscraper/bin/activate
 cd ~/webapps/busscraper
 scrapy crawl gotobus
